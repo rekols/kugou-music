0b9efe565581e0623d83defca808669dabc431df
rekols <rekols@foxmail.com>
2018-03-03 13:46:49 +0800

    set single instance.

--- main.cpp
+++ main.cpp
@@ -8,19 +8,30 @@ DWIDGET_USE_NAMESPACE
 int main(int argc, char *argv[])
 {
     DApplication::loadDXcbPlugin();
-    DApplication a(argc, argv);
+    DApplication app(argc, argv);
+    app.loadTranslator();
+    app.setOrganizationName("deepin");
+    app.setApplicationVersion(DApplication::buildVersion("1.0"));
+    app.setApplicationAcknowledgementPage("https://github.com/rekols");
+    app.setProductIcon(QIcon(":/images/icon.svg"));
+    app.setProductName(DApplication::translate("Main", "酷狗音乐"));
+    app.setApplicationDescription(DApplication::translate("Main", "本项目代码不用于商用，仅限研究和学习使用"));
 
-    MainWindow w;
-    w.show();
-    w.setWindowIcon(QIcon(":/images/icon.svg"));
-    w.move((QApplication::desktop()->width() - w.width()) / 2,
-           (QApplication::desktop()->height() - w.height()) / 2);
+    if (app.setSingleInstance("kugou-music")) {
+        MainWindow w;
+        w.show();
+        w.setWindowIcon(QIcon(":/images/icon.svg"));
+        w.move((QApplication::desktop()->width() - w.width()) / 2,
+               (QApplication::desktop()->height() - w.height()) / 2);
 
-    QFile file(":/qss/style.qss");
-    if (file.open(QIODevice::ReadOnly)) {
-        const QString qss = file.readAll();
-        w.setStyleSheet(qss);
+        QFile file(":/qss/style.qss");
+        if (file.open(QIODevice::ReadOnly)) {
+            const QString qss = file.readAll();
+            w.setStyleSheet(qss);
+        }
+
+        return app.exec();
     }
 
-    return a.exec();
+    return 0;
 }
